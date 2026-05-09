f9f50376b0543acb70f555a876a494484dc37abc
vinaypurbia <vinay.purbia@gmail.com>
2026-05-09 03:41:53 +0300

    Create [id].js

---
+++ api/testimonials/[id].js
@@ -0,0 +1,23 @@
+const { connectDB, Testimonial } = require("../_db");
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  await connectDB();
+  const { id } = req.query;
+
+  if (req.method === "PUT") {
+    const t = await Testimonial.findByIdAndUpdate(id, req.body, { new: true });
+    return res.json(t);
+  }
+
+  if (req.method === "DELETE") {
+    await Testimonial.findByIdAndDelete(id);
+    return res.json({ success: true });
+  }
+
+  res.status(405).json({ error: "Method not allowed" });
+};
