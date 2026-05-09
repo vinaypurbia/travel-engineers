f80c3d9f7c15d7add3db115734bbd4fa84d43f34
vinaypurbia <vinay.purbia@gmail.com>
2026-05-09 03:41:00 +0300

    Create [id].js

---
+++ api/rentals/[id].js
@@ -0,0 +1,23 @@
+const { connectDB, Rental } = require("../_db");
+
+module.exports = async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.status(200).end();
+
+  await connectDB();
+  const { id } = req.query;
+
+  if (req.method === "PUT") {
+    const rental = await Rental.findByIdAndUpdate(id, req.body, { new: true });
+    return res.json(rental);
+  }
+
+  if (req.method === "DELETE") {
+    await Rental.findByIdAndDelete(id);
+    return res.json({ success: true });
+  }
+
+  res.status(405).json({ error: "Method not allowed" });
+};
