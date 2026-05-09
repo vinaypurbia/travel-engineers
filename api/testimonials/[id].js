98eae856b4443cab928e7f358dee69b8e51849aa
vinaypurbia <vinay.purbia@gmail.com>
2026-05-09 10:59:57 +0300

    Update [id].js

--- api/testimonials/[id].js
+++ api/testimonials/[id].js
@@ -2,7 +2,7 @@ const { connectDB, Testimonial } = require("../_db");
 
 module.exports = async (req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "PUT,PATCH,DELETE,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   if (req.method === "OPTIONS") return res.status(200).end();
 
@@ -10,8 +10,13 @@ module.exports = async (req, res) => {
     await connectDB();
     const { id } = req.query;
 
-    if (req.method === "PUT") {
-      const t = await Testimonial.findByIdAndUpdate(id, req.body, { new: true });
+    if (req.method === "PUT" || req.method === "PATCH") {
+      const t = await Testimonial.findByIdAndUpdate(
+        id,
+        { $set: req.body },
+        { new: true, runValidators: false }
+      );
+      if (!t) return res.status(404).json({ error: "Not found" });
       return res.json(t);
     }
 
