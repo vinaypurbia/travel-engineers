51907d9d66516afe52ccead8e6c73d6bca5c2a9d
vinaypurbia <vinay.purbia@gmail.com>
2026-06-15 10:54:29 +0300

    Update TravelAssistant.jsx

--- src/TravelAssistant.jsx
+++ src/TravelAssistant.jsx
@@ -81,7 +81,7 @@ export default function TravelAssistant() {
     setLoading(true);
 
     try {
-      const res = await fetch("/api/chat", {
+      const res = await fetch("/api/upload?action=chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
